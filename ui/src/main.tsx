88471b1d3fdea7e229757da173a860274c5cc2fe
Sanjay George <sanjaygeorge16@gmail.com>
2024-12-05 08:43:57 +0100

    Use Memory Router (react router)
    To avoid updating the URL of opened website

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { NextUIProvider } from "@nextui-org/react";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { MemoryRouter, Route, Routes } from "react-router";
 import AnnotationEditorView from './views/AnnotationEditorView.tsx';
 import AnnotationListView from './views/AnnotationListView.tsx';
 
@@ -11,14 +11,14 @@ import AnnotationListView from './views/AnnotationListView.tsx';
 createRoot(document.getElementById('document-io-root')!).render(
   <StrictMode>
     <NextUIProvider>
-      <BrowserRouter>
+      <MemoryRouter>
         <Routes>
           <Route element={<App />}>
             <Route path="/" element={<AnnotationListView />} />
             <Route path="/editor/:id" element={<AnnotationEditorView />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </MemoryRouter>
     </NextUIProvider>
   </StrictMode>,
 )
